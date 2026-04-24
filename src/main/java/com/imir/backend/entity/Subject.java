405be57d746d9c660f89f02c1b6f6a5481b7ad06
Dmitrii <sde2424242424@gmai.com>
2026-04-24 21:56:09 +0900

    fix: 0.4

--- src/main/java/com/imir/backend/entity/Subject.java
+++ src/main/java/com/imir/backend/entity/Subject.java
@@ -1,12 +1,8 @@
 package com.imir.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 @Table(name = "subjects")
 public class Subject {
 
@@ -21,4 +17,39 @@ public class Subject {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public Subject() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
